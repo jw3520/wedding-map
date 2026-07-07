7b125a1fa846475555f750378041a22339eb6c06
jw3520 <jw3520@daum.net>
2026-07-07 12:35:43 +0900

    Redirect wedding map to wedding site

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: '/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: 'src/main.tsx',
-      output: {
-        entryFileNames: 'assets/index.js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: (assetInfo) =>
-          assetInfo.names?.some((name) => name.endsWith('.css'))
-            ? 'assets/index.css'
-            : 'assets/[name][extname]',
-      },
-    },
-  },
   plugins: [react()],
 });
